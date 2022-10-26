16d7085584cc1de0d09f582a8063314698efd14b
phantrunghieu91 <phantrunghieu1291@gmail.com>
2022-10-27 00:36:06 +0700

    axios services

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
-import axios from 'axios';
+import apiAxios from './services/apiAxios';
 import { useState } from 'react';
 import { useEffect } from 'react';
 
 function App() {
   const [user, setUser] = useState([]);
   useEffect(() => {
-    axios.get('https://peppermint-wary-ghoul.glitch.me/user').then(resp => {
-      setUser(resp.data);
+    apiAxios.get('/user').then(resp => {
+      setUser(resp);
     });
   }, []);
   return (
     <div className='container p-3'>
       <h1 className='text-center bg-black text-white text-[150px]'>Hello world</h1>
-      <ul>
+      <ol>
         {user &&
           user.map(item => (
             <li key={item.id} className='font-semibold'>
               {item.username}
             </li>
           ))}
-      </ul>
+      </ol>
       <button
         className='border border-black p-2'
         onClick={() => {
-          axios
-            .post('https://peppermint-wary-ghoul.glitch.me/user', { username: 'newsuser' })
-            .then(resp => {
-              setUser([...user, resp.data]);
-            });
+          apiAxios.post('/user', { username: 'testing' }).then(resp => {
+            setUser([...user, resp]);
+          });
         }}>
         Add new user
       </button>
